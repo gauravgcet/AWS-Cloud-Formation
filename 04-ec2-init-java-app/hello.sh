67bc618280f58e0aa0e44f84a5b4a51f7f7c7963
Gaurav <gaurav_gcet@yahoo.com>
2020-04-19 22:23:04 +0100

    ~

--- 04-ec2-init-java-app/hello.sh
+++ 04-ec2-init-java-app/hello.sh
@@ -1,2 +1,38 @@
 #!/bin/bash -xe
 java -jar /var/tmp/hello.jar &
+
+
+#!/bin/bash -xe
+# Source function library.
+. /etc/init.d/functions
+
+start() {
+    # code to start app comes here
+    # example: daemon program_name &
+}
+
+stop() {
+    # code to stop app comes here
+    # example: killproc program_name
+}
+
+case "$1" in
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+       stop
+       start
+       ;;
+    status)
+       # code to check status of app comes here
+       # example: status program_name
+       ;;
+    *)
+       echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
